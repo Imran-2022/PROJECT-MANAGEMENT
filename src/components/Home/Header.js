b354ec6b46558e4613c65c2073a171179afc8501
Imran-2022 <mdimranulhaque.2022@gmail.com>
2022-05-10 05:56:48 +0600

    header

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import "./Header.css"
 const Header = () => {
     return (
-        <div>
+        <div className="header">
             <img src="https://ict-innovation-fest-2021.mist.ac.bd/assets/img/project.jpeg" alt="" />
         </div>
     );
