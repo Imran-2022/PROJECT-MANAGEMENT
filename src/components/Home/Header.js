2cd5d657f8cdb66da2336b03a72e90a1ca5527b0
Imran-2022 <mdimranulhaque.2022@gmail.com>
2022-05-10 05:27:16 +0600

    starter project

---
+++ src/components/Home/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+
+            <img src="https://ict-innovation-fest-2021.mist.ac.bd/assets/img/project.jpeg" alt="" />
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Pages/Pages.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Pages.css"
+const Pages = () => {
+    return (
+        <div>
+            <p>All Pages</p>
+
+        </div>
+    );
+};
+
+export default Pages;
