06fb6241c07cbad3d53d08cf130f58f0b43f1306
Imran-2022 <mdimranulhaque.2022@gmail.com>
2022-05-10 15:27:41 +0600

    add react player

--- src/components/Projects/SingleProject.js
+++ src/components/Projects/SingleProject.js
@@ -3,19 +3,21 @@ import { useParams } from 'react-router-dom';
 import JsonData from "../../../src/Fake.json"
 import "./SingleProjects.css"
 import { Link } from "react-router-dom";
+import ReactPlayer from 'react-player'
 const SingleProject = () => {
     const { abc } = useParams()
     const [singleProject, setSingleProject] = useState(...JsonData.filter(x => x.id === abc));
-    const { id, description, imgUrl, project_name, title,frontEnd,BackEnd,deployment } = singleProject;
+    const { id, description, imgUrl, project_name, title, frontEnd, BackEnd, deployment,videoUrl } = singleProject;
     useEffect(() => {
         // move to top of page while click on link react js
         window.scrollTo(0, 0)
-      }, [])
+    }, [])
     return (
         <>
             <Link to="/" className="back-btn">Back to Home</Link>
             <div className="single-Project" key={id}>
-                <img src={imgUrl} alt="project-img" />
+                {/* <iframe className="single-project-image"  src={videoUrl} title="video"></iframe> */}
+                <ReactPlayer light={imgUrl}  className="single-project-image" playing={true} controls={true} loop={false} url={videoUrl} />
                 <div className="project-details">
                     <h3>Project</h3>
                     <p>{project_name}</p>
@@ -26,49 +28,49 @@ const SingleProject = () => {
                     <h4>Front-end</h4>
 
                     <div className="fronend">
-                    {
-                        frontEnd.split(",").map((d,idx)=>{
-                            return(
-                                <p key={idx}> {d}</p>
-                            )
-                        })
-                    }
+                        {
+                            frontEnd.split(",").map((d, idx) => {
+                                return (
+                                    <p key={idx}> {d}</p>
+                                )
+                            })
+                        }
                     </div>
                     <h4>Back-end</h4>
                     <div className="backend">
-                    {
-                        BackEnd.split(",").map((d,idx)=>{
-                            return(
-                                <p key={idx}> {d}</p>
-                            )
-                        })
-                    }
+                        {
+                            BackEnd.split(",").map((d, idx) => {
+                                return (
+                                    <p key={idx}> {d}</p>
+                                )
+                            })
+                        }
                     </div>
                     <h4>deployment</h4>
                     <div className="deployment">
-                    {
-                        deployment.split(",").map((d,idx)=>{
-                            return(
-                                <p key={idx}> {d}</p>
-                            )
-                        })
-                    }
+                        {
+                            deployment.split(",").map((d, idx) => {
+                                return (
+                                    <p key={idx}> {d}</p>
+                                )
+                            })
+                        }
                     </div>
                     {/* description */}
                     <h3>Descriptiion :</h3>
                     <h4>What user can do ?</h4>
                     {
-                        description.split("||").map((d,idx)=>{
-                            return(
-                                <p key={idx}> {idx+1} . {d}</p>
+                        description.split("||").map((d, idx) => {
+                            return (
+                                <p key={idx}> {idx + 1} . {d}</p>
                             )
                         })
                     }
                     <h4>What only Admin can do ?</h4>
                     {
-                        description.split("||").map((d,idx)=>{
-                            return(
-                                <p key={idx}> {idx+1} . {d}</p>
+                        description.split("||").map((d, idx) => {
+                            return (
+                                <p key={idx}> {idx + 1} . {d}</p>
                             )
                         })
                     }
