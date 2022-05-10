5e488a2b134f72ab2f05d7cf415ec527618657e1
Imran-2022 <mdimranulhaque.2022@gmail.com>
2022-05-10 06:53:43 +0600

    project page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Projects.css"
+import JsonData from "../../../src/Fake.json"
 const Projects = () => {
+    const [projects, setProjects] = useState(JsonData.slice(0, 20));
     return (
-        <div>
-            <p>this is project Pages.</p>
+        <div className="projects">
+            {
+                projects.map((dt) =>{
+                    const {id,description,imgUrl,project_name,title}=dt;
+                    console.log(id,description,imgUrl,project_name,title);
+                    return (
+                        <div className="project" key={id}>
+                            <img src={imgUrl} alt="project-img" />
+                            <div className="project-details">
+                                <p>Project : {project_name.slice(0,20)}</p>
+                                <p>Title : {title.slice(0,60)}</p>
+                                <p>description : {description.slice(0,100)}</p>
+                                <button className="project-btn">Details here</button>
+                               {/* <div className="project-btn">
+                               <button className="btn">Details</button>
+                               <button className="btn">Details</button>
+                               </div> */}
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     );
 };
--- src/components/Projects/SingleProject.js
+++ src/components/Projects/SingleProject.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import "./Projects.css"
 const SingleProject = () => {
+    const {id} = useParams()
+    console.log(id)
     return (
         <div>
             <p>here project Details</p>
